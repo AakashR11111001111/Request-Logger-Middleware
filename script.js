a6fd49799deba268edb012010bf4acc5726afe40
AakashR11111001111 <aakash04721202019@msi-ggsip.org>
2025-02-25 22:01:51 +0530

    added files

---
+++ script.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next)=>{
+    req.execution = Date.now();
+    if(true){  
+        next();
+    }
+})
+
+app.get("/api/request-logger",(req, res)=>{
+    res.json({
+        success: true,
+        message: {
+            RequestMethod: req.method,
+            url: `${req.url}`,
+            ip: "::1" ? "127.0.0.1" : req.ip,
+            ExecutedIn: `${Date.now() - req.execution}ms`
+
+        }
+    }) 
+})
+
+app.listen(1000)
